11ed68fdf3989c1fc583309eb1203e4aeecab111
Gregory Ronin <gregoryronin@yahoo.com>
2015-05-19 14:41:05 -0700

    Cleanup.

--- mgorest/v1_0/models.py
+++ mgorest/v1_0/models.py
@@ -1,22 +1,4 @@
-
 from mgorest import db
-
-#from mgo.app_init import app
-
-#print 'models:', app.config['SQLALCHEMY_DATABASE_URI']
-
-
-#class Sighting(db.Model):
-#  __tablename__ = 'sightings'
-#  id = db.Column(db.Integer, primary_key = True)
-#  sighted_at = db.Column(db.Integer)
-#  reported_at = db.Column(db.Integer)
-#  location = db.Column(db.String(100))
-#  shape = db.Column(db.String(10))
-#  duration = db.Column(db.String(10))
-#  description = db.Column(db.Text)
-#  lat = db.Column(db.Float(6))
-#  lng = db.Column(db.Float(6))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
